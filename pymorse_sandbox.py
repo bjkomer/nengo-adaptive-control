d2600359fa996c107c045a0f330e846014afb3be
Brent Komer <brent.komer@gmail.com>
2013-10-11 12:56:38 -0400

    adding odometry reading to sandbox

--- pymorse_sandbox.py
+++ pymorse_sandbox.py
@@ -1,4 +1,5 @@
 import pymorse
+import time
 
 torque_out = { "data":[ { "component":"segment_1",
                           "force":[ 0, 0, 0 ],
@@ -7,10 +8,18 @@ torque_out = { "data":[ { "component":"segment_1",
                           "force":[ 0, 0, 0 ],
                           "torque":[ 0, 0, 0] } ] }
 
+def odom_reading( odom ):
+  print( odom )
+
 with pymorse.Morse() as sim:
   #print( dir(sim) )
   #print( sim.get_stream_port(sim.streams()[0]) )
   #print( dir(sim.arm.torque) )
+
+  sim.arm.odom.subscribe( odom_reading )
+
   torque_out['data'][0]['torque'][0] = 2
   torque_out['data'][1]['torque'][0] = 5
   sim.arm.torque.publish( torque_out )
+
+  time.sleep(0.1)
