47ef31425461ccda5d99eece5bd747c9f14d9155
Brent Komer <brent.komer@gmail.com>
2013-10-11 12:49:56 -0400

    typo fix

--- pymorse_sandbox.py
+++ pymorse_sandbox.py
@@ -5,12 +5,12 @@ torque_out = { "data":[ { "component":"segment_1",
                           "torque":[ 0, 0, 0 ] },
                         { "component":"segment_2",
                           "force":[ 0, 0, 0 ],
-                          "torque":[ 0, 0, 0] } ] } )
+                          "torque":[ 0, 0, 0] } ] }
 
 with pymorse.Morse() as sim:
-  print( dir(sim) )
-  print( sim.get_stream_port(sim.streams()[0]) )
-  print( dir(sim.arm.torque) )
+  #print( dir(sim) )
+  #print( sim.get_stream_port(sim.streams()[0]) )
+  #print( dir(sim.arm.torque) )
   torque_out['data'][0]['torque'][0] = 2
   torque_out['data'][1]['torque'][0] = 5
   sim.arm.torque.publish( torque_out )
